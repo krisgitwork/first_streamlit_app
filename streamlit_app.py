90d00c91e55c5c561163fda813b311e4b9e34d37
krisgitwork <110132127+krisgitwork@users.noreply.github.com>
2022-07-28 14:49:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My first github work')
+streamlit.title('My Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale Spinach Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
